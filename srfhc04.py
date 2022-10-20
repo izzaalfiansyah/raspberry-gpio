e52b846895d3c864fadd987d55c81fe16114bdd6
izzaalfiansyah <iansyah724@gmail.com>
2022-10-20 03:59:00 +0100

    add srfhc04

--- srfhc04.py
+++ srfhc04.py
@@ -10,3 +10,24 @@ echo = 16
 GPIO.setup(trigger, GPIO.OUT)
 GPIO.setup(echo, GPIO.IN)
 GPIO.output(trigger, GPIO.LOW)
+
+def get_range():
+	GPIO.output(trigger, True)
+	time.sleep(0.00001)
+	GPIO.output(trigger, False)
+	timeout_counter = int(time.time())
+	start = time.time()
+
+	while GPIO.input(echo) == 0 and (int(time.time()) - timeout_counter) < 3:
+		stop = time.time()
+
+	elapsed = stop-start
+	distance = elapsed * 34320
+	distance = distance / 2
+
+	return distance
+
+while True:
+	jarak = get_range()
+	print('Jarak terukur : %.2f Cm' % jarak)
+	time.sleep(1)
